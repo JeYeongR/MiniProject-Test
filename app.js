e073464d6c8a393630f7f9336eb73f418969cadf
JeYeongR <wpdud2003@gmail.com>
2023-09-07 20:42:33 +0900

    Feature: sign-up

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const http = require("http");
 const express = require("express");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
@@ -22,3 +23,60 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+const signUp = async (req, res) => {
+  try {
+    const { name, email, profileImage = "기본 이미지", password } = req.body;
+
+    if (!name || !email || !password) {
+      const error = new Error("KEY_ERROR");
+      error.status = 400;
+      throw error;
+    }
+
+    if (password.length < 8) {
+      const error = new Error("INVALID_PASSWORD");
+      error.status = 400;
+      throw error;
+    }
+
+    const [existingUser] = await myDataSource.query(`SELECT email FROM users WHERE email = '${email}'`);
+    if (existingUser) {
+      const error = new Error("DUPLICATED_EMAIL_ADDRESS");
+      error.status = 400;
+      throw error;
+    }
+
+    const hasSpecialChar = /[\{\}\[\]\/?.,;:|\)*~`!^\-_+<>@\#$%&\\\=\(\'\"]/g;
+    if (!hasSpecialChar.test(password)) {
+      const error = new Error("NO_SPECIAL_CHARACTERS");
+      error.status = 400;
+      throw error;
+    }
+
+    const encryptedPw = bcrypt.hashSync(password, 2);
+
+    await myDataSource.query(
+      `INSERT INTO users (name, email,profile_image, password) VALUES ('${name}', '${email}', '${profileImage}', '${encryptedPw}')`
+    );
+
+    return res.status(201).json({ message: "userCreated" });
+  } catch (error) {
+    console.log(error);
+    return res.status(error.status).json({ message: error.message });
+  }
+};
+
+app.post("/sign-up", signUp);
+
+const server = http.createServer(app);
+
+const start = async () => {
+  try {
+    server.listen(8000);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
